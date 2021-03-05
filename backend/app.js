0c1d83c62fd4751829f49591d7a4015ae20df157
Daniel <danielc@runninghill.co.za>
2021-03-05 07:52:12 +0200

    Get and post methods for mongoDB implemented.

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Post = require('./models/post');
 
 const app = express();
+
+mongoose.connect("mongodb+srv://Daniel:53EENEwfLNDwOf58@cluster0.xl55b.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to DB');
+  }).catch(() => {
+    console.log('Connection failed');
+});
 
 app.use(bodyParser.json());
 
@@ -13,30 +23,25 @@ app.use((req, res, next) => {
 });
 
 app.post('/api/posts', (req, res, next) => {
-  const post = req.body;
-  console.log(post);
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+  post.save();
   res.status(201).json({
     message: 'Post added successfully',
   });
 });
 
 app.get('/api/posts', (req, res, next) => {
-  const posts = [
-    {
-      id: '123',
-      title: 'First Post',
-      content: 'Coming from server'
-    },
-    {
-      id: '456',
-      title: 'Second Post',
-      content: 'Coming from server too'
-    },
-  ];
-  res.status(200).json({
-    message: 'Posts fetched successfully!',
-    posts: posts
-  });
+  Post.find().then(
+    documents => {
+      res.status(200).json({
+        message: 'Posts fetched successfully!',
+        posts: documents
+      });
+    }
+  );
 });
 
 module.exports = app;
